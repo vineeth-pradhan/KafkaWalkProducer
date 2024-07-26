31d99866b981cfd22165b2935cba46c238977759
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-07-26 05:44:33 +0530

    Streaming wikimedia

--- WikimediaHttp3Stream/build.gradle.kts
+++ WikimediaHttp3Stream/build.gradle.kts
@@ -11,8 +11,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.apache.kafka:kafka-clients:3.8.0")
+    implementation("org.slf4j:slf4j-api:2.0.16")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
+    implementation("com.squareup.okhttp3:okhttp:3.14.9")
+    implementation("com.launchdarkly:okhttp-eventsource:4.1.1")
 }
 
 tasks.test {
--- WikimediaHttp3Stream/src/main/java/com/tollfreeroad/kafkawalk/Producer.java
+++ WikimediaHttp3Stream/src/main/java/com/tollfreeroad/kafkawalk/Producer.java
@@ -1,7 +1,42 @@
 package com.tollfreeroad.kafkawalk;
 
+import com.launchdarkly.eventsource.MessageEvent;
+import okhttp3.HttpUrl;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.launchdarkly.eventsource.EventSource;
+
+import java.util.Properties;
+
 public class Producer {
+    private static final Logger log = LoggerFactory.getLogger(Producer.class.getSimpleName());
+
     public static void main(String[] args) {
-        System.out.println("HTTP3Stream Hello");
+        Properties props = new Properties();
+        // Server properties
+        props.setProperty("bootstrap.servers", "localhost:9094");
+
+        // Producer properties
+        props.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        // Get producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        String topic = "wikimedia.recentchange";
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("https")
+                .host("stream.wikimedia.org")
+                .addPathSegment("v2")
+                .addPathSegment("stream")
+                .addPathSegment("recentchange")
+                .build();
+        EventSource builder = new EventSource.Builder(url).build();
+        log.info("All went well");
+        log.info(url.toString());
+        for(MessageEvent m: builder.messages()) {
+            log.info(m.getData());
+        }
     }
 }
