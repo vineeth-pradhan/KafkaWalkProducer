6c7ce2249ab1081f27000f5d0ea0314b2a8834d0
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2024-08-08 05:44:03 +0530

    Some recommended production configuration

--- WikimediaHttp3Stream/src/main/java/com/tollfreeroad/kafkawalk/Producer.java
+++ WikimediaHttp3Stream/src/main/java/com/tollfreeroad/kafkawalk/Producer.java
@@ -3,14 +3,12 @@ package com.tollfreeroad.kafkawalk;
 import com.launchdarkly.eventsource.MessageEvent;
 import com.launchdarkly.eventsource.StreamException;
 import okhttp3.HttpUrl;
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.errors.AuthorizationException;
 import org.apache.kafka.common.errors.OutOfOrderSequenceException;
 import org.apache.kafka.common.errors.ProducerFencedException;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.launchdarkly.eventsource.EventSource;
@@ -22,14 +20,21 @@ public class Producer {
 
     public static void main(String[] args) {
         Properties props = new Properties();
-        // Server properties
-        props.setProperty("bootstrap.servers", "localhost:9094");
 
-        // Producer properties
-        props.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+//      Server properties ================================
+//      props.setProperty("bootstrap.servers", "localhost:9094");
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094");
 
-        // Get producer
+//      Producer properties ==============================
+//      props.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getCanonicalName());
+//      props.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getCanonicalName());
+        props.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        props.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+        props.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "lz4");
+
+//      Get producer =====================================
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
 
         String topic = "wikimedia.recentchange";
@@ -41,11 +46,12 @@ public class Producer {
                 .addPathSegment("recentchange")
                 .build();
         EventSource builder = new EventSource.Builder(url).build();
+
+//      Stream to producer ===============================
         try {
             builder.start();
             for(MessageEvent m: builder.messages()) {
                 ProducerRecord<String, String> record = new ProducerRecord<>(topic, "oct-2024", m.getData());
-                log.info(m.getData());
                 producer.send(record, new Callback() {
                     @Override
                     public void onCompletion(RecordMetadata recordMetadata, Exception e) {
